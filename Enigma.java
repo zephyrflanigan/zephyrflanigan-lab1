02a0d11fff1593a39052c2bcca6c5a10515eb669
Zephyr2 <zephyr2@Zephyr-2.local>
2024-02-07 11:14:06 -0500

    finished rotor and enigma classes

--- Enigma.java
+++ Enigma.java
@@ -1,4 +1,4 @@
-public class Enigma{
+public class Enigma {
 
     private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
@@ -21,14 +21,59 @@ public class Enigma{
 
     public String decrypt(String message){        
         //TODO
+        String decryptedMessage = "";
+        for(int i = 0; i < message.length(); i++){
+
+            //find character placement in the outer
+            char charDecrypt = message.charAt(i);
+            int outerValue = rotors[2].indexOf(charDecrypt);
+
+            //find character at this value in middle rotor
+            char charMiddle = rotors[1].charAt(outerValue);
+
+            //find character placement in outer rotor again
+            int secondOuter = rotors[2].indexOf(charMiddle);
+
+            /*finally, find corresponding inner char and add this
+            to decrypted String */
+            char decrypted = rotors[0].charAt(secondOuter);
+            decryptedMessage = decryptedMessage + decrypted;
+
+            //call rotate
+            rotate();
+        }
+        return decryptedMessage;
+        
     }
 
 
     
     public String encrypt(String message){
         //TODO
+        String encryptedMessage = "";
+        for(int i = 0; i < message.length(); i++){
 
-    
+            //find character placement in the inner rotor
+            char charEncrypt = message.charAt(i);
+            int innerValue = rotors[0].indexOf(charEncrypt);
+
+            //find character at this value in outer rotor
+            char charOuter = rotors[2].charAt(innerValue);
+
+            //find character placement in middle rotor
+            int middleValue = rotors[1].indexOf(charOuter);
+
+            /*finally, find corresponding outer char and add this
+            to encrypted String */
+            char encrypted = rotors[2].charAt(middleValue);
+            encryptedMessage = encryptedMessage + encrypted;
+
+            //call rotate
+            rotate();
+        }
+        return encryptedMessage;
+    }
+
     private void rotate(){
         if(rotors[0].rotate()){
             if(rotors[1].rotate()){
@@ -38,3 +83,4 @@ public class Enigma{
     }
     
 }
+
--- Rotor.java
+++ Rotor.java
@@ -14,16 +14,36 @@ public class Rotor {
     
     public boolean rotate(){
         //TODO
-               
+
+        //"rotate" rotor so previous final value is at the start of the string
+        rotorValues = rotorValues.charAt(26) + rotorValues.substring(0,26);
+        if(rotorValues.charAt(0) == startChar) {
+
+            //return true when rotor is at original position
+            return true;
+        }
+        return false;
+        
     }
     
 
     public int indexOf(char c){
         //TODO
+
+        //searches through rotor for the inputted charcter
+        for(int i = 0; i < 27; i++) {
+            if(rotorValues.charAt(i) == c) {
+                return i;
+            }
+        }
+        //in the case the input is not a character in the rotor
+        return -1;
     }
 
     public char charAt(int idx){
         //TODO
+        //returns character at a given index in the rotor
+        return rotorValues.charAt(idx);
     }
 }
     
